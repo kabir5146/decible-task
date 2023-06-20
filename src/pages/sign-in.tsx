8725a5512f682edcfc6f77d50e9a6287af32939f
kabir5146 <fl.kabirhossain@gmail.com>
2023-06-20 19:48:45 +0600

    page center align done

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -22,25 +22,34 @@ const SignInPage = () => {
   return (
     <div
       style={{
-        width: "400px",
-        borderRadius: "4px",
-        padding: "16px",
-        background: "#c1c1c1",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
       }}
     >
-      <Form layout="vertical" onFinish={handleSubmit}>
-        <Form.Item label="Email">
-          <Input type="email" value={email} onChange={handleEmailChange} />
-        </Form.Item>
-        <Form.Item label="Password">
-          <Input.Password value={password} onChange={handlePasswordChange} />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Sign In
-          </Button>
-        </Form.Item>
-      </Form>
+      <div
+        style={{
+          width: "400px",
+          borderRadius: "4px",
+          padding: "16px",
+          background: "#c1c1c1",
+        }}
+      >
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item label="Email">
+            <Input type="email" value={email} onChange={handleEmailChange} />
+          </Form.Item>
+          <Form.Item label="Password">
+            <Input.Password value={password} onChange={handlePasswordChange} />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Sign In
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </div>
   );
 };
