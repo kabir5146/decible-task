4d6f1316801051351bf3f1e9422736c8858ba87b
kabir5146 <fl.kabirhossain@gmail.com>
2023-06-20 15:41:28 +0600

    first comit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,217 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Table, Button } from "antd";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+const dataSource = [
+  {
+    callType: "Voice Mail",
+    direction: "Outbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Archived",
+  },
+  {
+    callType: "Answered",
+    direction: "Inbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Unarchive",
+  },
+  {
+    callType: "Missed",
+    direction: "Outbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Unarchive",
+  },
+  {
+    callType: "Voice Mail",
+    direction: "Outbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Archived",
+  },
+  {
+    callType: "Answered",
+    direction: "Inbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Unarchive",
+  },
+  {
+    callType: "Missed",
+    direction: "Outbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Unarchive",
+  },
+  {
+    callType: "Voice Mail",
+    direction: "Outbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Archived",
+  },
+  {
+    callType: "Answered",
+    direction: "Inbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Unarchive",
+  },
+  {
+    callType: "Missed",
+    direction: "Outbound",
+    duration: "80 minutes 23 seconds",
+    durationSeconds: "4823 seconds",
+    from: "+33148288105",
+    to: "+33166114113",
+    via: "+33148288105",
+    createdAt: "12-082022",
+    status: "Unarchive",
+  },
+];
+
+const columns = [
+  // 1st column
+  {
+    title: "CALL TYPE",
+    dataIndex: "callType",
+    key: "callType",
+    render: (text, record) => (
+      <span
+        style={{
+          color:
+            record.callType === "Missed"
+              ? "#ef4444"
+              : record.callType === "Voice Mail"
+              ? "#9144ef"
+              : "#18ffc9",
+        }}
+      >
+        {text}
+      </span>
+    ),
+  },
+  // 2nd column
+  {
+    title: "DIRECTION",
+    dataIndex: "direction",
+    key: "direction",
+    render: (text) => <span style={{ color: "#9144ef" }}>{text}</span>,
+  },
+  {
+    title: "DURATION",
+    dataIndex: "duration",
+    key: "duration",
+    render: (text, record) => (
+      <>
+        <p>{text}</p>
+        <p style={{ color: "#9144ef" }}>{record.durationSeconds}</p>
+      </>
+    ),
+  },
+  {
+    title: "FORM",
+    dataIndex: "from",
+    key: "from",
+    render: (text) => <span>{text}</span>,
+  },
+  {
+    title: "TO",
+    dataIndex: "to",
+    key: "to",
+    render: (text) => <span>{text}</span>,
+  },
+  {
+    title: "VIA",
+    dataIndex: "via",
+    key: "via",
+    render: (text) => <span>{text}</span>,
+  },
+  {
+    title: "CREATED AT",
+    dataIndex: "createdAt",
+    key: "createdAt",
+    render: (text) => <span>{text}</span>,
+  },
+  {
+    title: "STATUS",
+    dataIndex: "status",
+    key: "status",
+    render: (text, record) => (
+      <Button
+        style={{
+          color: record.callType === "Answered" ? "#000000" : "#18ffc9",
+          background:
+            record.callType === "Answered" ? "#bdbdbd4c" : "#60ffef2a",
+          border: 0,
+        }}
+      >
+        {text}
+      </Button>
+    ),
+  },
+  {
+    title: "ACTIONS",
+    key: "actions",
+    render: () => (
+      <Button
+        style={{
+          color: "#ffffff",
+          background: "#000000",
+          border: 0,
+        }}
+        className=""
+      >
+        Add Note
+      </Button>
+    ),
+  },
+];
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="">
+          <Table dataSource={dataSource} columns={columns} />
         </div>
       </main>
     </>
-  )
+  );
 }
